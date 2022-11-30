e73fac8f1cbc05976452b408da98bd19d14be6c9
Ethan <neh9483@uncw.edu>
2022-11-30 01:15:02 -0500

    bad generation algorithm, trying new one

--- scripts/board.gd
+++ scripts/board.gd
@@ -14,47 +14,96 @@ func _init():
 
 
 func generate():
-	rows = [
-		[1, 2, 3,	4, 5, 6,	7, 8, 9],	# begin with trivial board
-		[4, 5, 6,	7, 8, 9,	1, 2, 3],
-		[7, 8, 9,	1, 2, 3,	4, 5, 6],
-		
-		[2, 3, 1,	5, 6, 4,	8, 9, 7],
-		[5, 6, 4,	8, 9, 7,	2, 3, 1],
-		[8, 9, 7,	2, 3, 1,	5, 6, 4],
-		
-		[3, 1, 2,	6, 4, 5,	9, 7, 8],
-		[6, 4, 5,	9, 7, 8,	3, 1, 2],
-		[9, 7, 8,	3, 1, 2,	6, 4, 5],
-	]
+#	rows = [
+#		[1, 2, 3,	4, 5, 6,	7, 8, 9],	# begin with trivial board
+#		[4, 5, 6,	7, 8, 9,	1, 2, 3],
+#		[7, 8, 9,	1, 2, 3,	4, 5, 6],
+#
+#		[2, 3, 1,	5, 6, 4,	8, 9, 7],
+#		[5, 6, 4,	8, 9, 7,	2, 3, 1],
+#		[8, 9, 7,	2, 3, 1,	5, 6, 4],
+#
+#		[3, 1, 2,	6, 4, 5,	9, 7, 8],
+#		[6, 4, 5,	9, 7, 8,	3, 1, 2],
+#		[9, 7, 8,	3, 1, 2,	6, 4, 5],
+#	]
+#
+#	shuffle()
+	rows = random_rows()
 	
-	shuffle()
-	
+	solutions = [null]
 	for x in rows:			#load into solutions
 		x.insert(0, null)	#1-indexing
 		solutions.append(x)
+	
+	cols = _rows_to_cols(rows)
+	# populate boxes
 	
 	for x in solutions:
 		print(x)
 
 
-func shuffle():
-	var nums = [0, 1, 2]
-	nums.shuffle()
-
-	#row_box_swap(board, nums)
-	#col_box_swap(board, nums)
-	shuffle_rows()
-	swap_values()
+func random_rows():
+	var rands = []
+	for i in range(9):
+		rands.append(range(9))
 	
-	#nums.shuffle()
-	#for i in range(3):
-	#	col_box_swap(board, i, nums[i])
+	for num in range(1, 10):
+		var available_rows = range(9)
+		var available_cols = range(9)
+		
+		for i in range(9):
+			if i < 8:
+				available_rows.shuffle()
+				available_cols.shuffle()
+				#var rnd = randi() % len(available_rows)
+			
+			var row_pos = available_rows.pop_back()
+			var col_pos = available_cols.pop_back()
+			remove_box(available_rows, row_pos)
+			remove_box(available_cols, col_pos)
+			
+			rands[row_pos][col_pos] = num
+	
+	for r in rands:
+		print(r)
+	
+	return rands
 
 
-func swap_values():
+func remove_box(arr, item):
+	# removes numbers in array associated with same box
+	# e.g. if index is 4, remove (3, 4, 5)
+	# if index is 0, remove (0, 1, 2)
+	if item in [0, 1, 2]:
+		arr.remove(0)
+		arr.remove(1)
+		arr.remove(2)
+	
+	elif item in [3, 4, 5]:
+		arr.remove(3)
+		arr.remove(4)
+		arr.remove(5)
+	
+	elif item in [6, 7, 8]:
+		arr.remove(6)
+		arr.remove(7)
+		arr.remove(8)
+
+
+func shuffle():
+	shuffle_box_cols()	# randomly order 9x3 cols
+	shuffle_box_rows()	# randomly order 9x3 rows
+	shuffle_cols()		# randomly order each set of 3 cols
+	shuffle_rows()		# randomly order each set of 3 rows
+	shuffle_values()	# randomly swap pairs of cell values
+	# ^ bad solution
+
+
+func shuffle_values():
 	var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 	nums.shuffle()
+	
 	for n in nums:
 		if n-1 != nums[n-1]:
 			value_swap(n, nums[n-1])
@@ -64,7 +113,7 @@ func value_swap(val1, val2):
 	if val1 == val2:
 		return
 	
-	print('swapping ', val1, ' with ', val2)
+	#print('swapping ', val1, ' with ', val2)
 	for row in rows:
 		for i in range(9):
 			if row[i] == val1:
@@ -74,62 +123,68 @@ func value_swap(val1, val2):
 				row[i] = val1
 
 
-func row_swap(r1, r2):
-	print('swapping ', r1, ' with ', r2)
-	var temp = rows[r1]
-	rows[r1] = rows[r2]
-	rows[r2] = temp
-
-
-func row_box_swap(rnd):
-	var rows_copy = rows.duplicate()
-	for i in range(3):
-		for j in range(3):
-			rows[i*3 + j] = rows_copy[rnd[i]*3 + j]
-			#print('row ', i*3 + j, ' = row ', rnd[i]*3 + j)
-
-
-func col_box_swap(rnd):
-	var columns = []
-	for i in range(9):
-		columns.append([])
-		for j in range(9):
-			columns[i].append(rows[j][i])
-	
-	var columns_copy = columns.duplicate()
-	for i in range(3):
-		for j in range(3):
-			columns[i*3 + j] = columns_copy[rnd[i]*3 + j]
-			print('column ', i*3 + j, ' = column ', rnd[i]*3 + j)
-	
-	for i in range(9):
-		for j in range(9):
-			rows[j][i] = columns[i][j]
-
-
-func shuffle_rows():
-	for i in [0, 3, 6]:
-		shuffle_row(i)
-
-
-func shuffle_row(box_ind):
+func shuffle_box_rows():
 	var rnd = [0, 1, 2]
 	rnd.shuffle()
 	print(rnd)
 	
-	var original = []
-	for i in range(3):
-		original.append(rows[box_ind + i])
+	var rows_copy = rows.duplicate()
 	
 	for i in range(3):
-		rows[box_ind + i] = original[rnd[i]]
+		for j in range(3):
+			rows[i*3 + j] = rows_copy[rnd[i]*3 + j]
+
+
+func shuffle_box_cols():
+	var rnd = [0, 1, 2]
+	rnd.shuffle()
+	print(rnd)
+	
+	cols = _rows_to_cols(rows)
+	var cols_copy = cols.duplicate()
+	
+	for i in range(3):
+		for j in range(3):
+			cols[i*3 + j] = cols_copy[rnd[i]*3 + j]
+	
+	rows = _rows_to_cols(cols)
+
+
+func shuffle_rows():
+	for i in [0, 3, 6]:
+		var rnd = [0, 1, 2]
+		rnd.shuffle()
+		print(rnd)
+		
+		var original = []
+		for j in range(3):
+			original.append(rows[i + j])
+		
+		for j in range(3):
+			rows[i + j] = original[rnd[j]]
 
 
 func shuffle_cols():
-	var columns = get_columns(rows)
-	for i in range(3):
-		pass
+	for i in [0, 3, 6]:
+		var rnd = [0, 1, 2]
+		rnd.shuffle()
+		print(rnd)
+		cols = _rows_to_cols(rows)
+		
+		var original = []
+		for j in range(3):
+			original.append(cols[i + j])
+		
+		for j in range(3):
+			cols[i + j] = original[rnd[j]]
+		
+		rows = _rows_to_cols(cols)
 
 
-func get_columns(board):
-	pass
+func _rows_to_cols(old):
+	var new = []
+	for i in range(9):
+		new.append([])
+		for j in range(9):
+			new[i].append(old[j][i])
+	return new
